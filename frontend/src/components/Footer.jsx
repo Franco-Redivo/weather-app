f8c031271dbf5cf8085de2a7c0328b6fa6c832ab
Franco Redivo <franco161101@gmail.com>
2026-04-26 16:46:17 -0300

    feat: add Footer component to display developer information and organization description

---
+++ frontend/src/components/Footer.jsx
@@ -0,0 +1,18 @@
+
+
+const Footer = () => {
+
+    return (
+        <footer className="bg-slate-950 text-white py-6">
+            <p className="text-sm">
+                <strong>Developed by:</strong> Franco Redivo
+            </p>
+
+            <p className="text-sm">
+                PM Accelerator is an organization focused on helping individuals break into product management by providing practical experience, mentorship, and exposure to real-world product challenges.
+            </p>
+        </footer>
+    )
+}
+
+export default Footer;
