780dc3844d34101d06c4fa5773b69ab22e59f50a
Franco Redivo <franco161101@gmail.com>
2026-04-26 16:46:06 -0300

    feat: update weather forecast API call to include precipitation probability and max wind speed

--- backend/src/services/weather.service.js
+++ backend/src/services/weather.service.js
@@ -21,7 +21,7 @@ export const getCurrentWeather = async (latitude, longitude) => {
 
 export const getWeatherForecast = async (latitude, longitude) => {
     try {
-        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&current=temperature_2m,apparent_temperature,is_day,precipitation,relative_humidity_2m,weather_code,wind_speed_10m,wind_direction_10m&timezone=auto&past_days=0&forecast_days=5`
+        const url = `https://api.open-meteo.com/v1/forecast?latitude=${latitude}&longitude=${longitude}&daily=weather_code,temperature_2m_max,temperature_2m_min,precipitation_sum,precipitation_probability_max,wind_speed_10m_max&hourly=temperature_2m,relative_humidity_2m,apparent_temperature,weather_code,wind_speed_10m&current=temperature_2m,apparent_temperature,is_day,precipitation,relative_humidity_2m,weather_code,wind_speed_10m,wind_direction_10m&timezone=auto&past_days=0&forecast_days=5`
         const response = await axios.get(url);
         return {
             current: response.data.current,
